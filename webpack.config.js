834039ea7361e58179a6d755bf3558d6d839044c
Hamit Zor <thenrerise@gmail.com>
2019-01-29 21:04:47 +0300

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+
+module.exports = function (env, argv) {
+  var configObject = {
+    output: {
+      filename: 'bundle.[contenthash].js',
+      publicPath: '/',
+    },
+    module: {
+      rules: [
+        { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+      ]
+    },
+    resolve: {
+      alias: {
+        '@material-ui/core': '@material-ui/core/es'
+      }
+
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        title: 'WYSIWYG Editor for React'
+      })
+    ],
+    devServer: {
+      open: true,
+      compress: true,
+      port: 4000,
+      publicPath: '/',
+      historyApiFallback: {
+        index: '/index.html'
+      }
+    }
+  }
+
+  if (argv.mode === 'development') {
+    configObject.devtool = 'cheap-module-source-map'
+  }
+
+  return configObject
+}
